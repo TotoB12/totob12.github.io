20f1c8134e1e9064f9b6da3f3f5e0952343cd094
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2022-11-08 12:25:15 -0500

    Update main.js

--- things/game/js/main.js
+++ things/game/js/main.js
@@ -1,13 +1,3 @@
-// Hello there!
-//
-// If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#1791
-
-console.warn(
-  "%cNote!",
-  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io\nPlease do not just add them without asking me first! Thank you!"
-);
-
 function script(text) {
   console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
 }
